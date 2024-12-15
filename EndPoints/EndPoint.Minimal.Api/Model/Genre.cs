b1c0e069a85eb69a24eb533e18ef5b06f792fe14
discoverbasecode <discoverbasecode@gmail.com>
2024-12-15 20:35:34 +0330

    Add Model Genre in Model

---
+++ EndPoints/EndPoint.Minimal.Api/Model/Genre.cs
@@ -0,0 +1,8 @@
+﻿namespace EndPoint.Minimal.Api.Model;
+
+public class Genre(string name, string description)
+{
+    public Guid Id { get; set; } = new();
+    public string Name { get; set; } = name;
+    public string Description { get; set; } = description;
+}
