724f3815660344f8e423e833eb959f08a6265d08
discoverbasecode <discoverbasecode@gmail.com>
2024-12-15 22:40:24 +0330

    add scalar documents api and config in endpoint api project

--- EndPoints/EndPoint.Minimal.Api/EndPoints/GenreEndPoints.cs
+++ EndPoints/EndPoint.Minimal.Api/EndPoints/GenreEndPoints.cs
@@ -1,5 +1,6 @@
 ﻿using EndPoint.Minimal.Api.Model;
 using EndPoint.Minimal.Api.Services.GenreServices;
+using Microsoft.AspNetCore.OutputCaching;
 
 namespace EndPoint.Minimal.Api.EndPoints;
 
@@ -8,11 +9,12 @@ public class GenreEndpoints
     public static void MapEndpoints(WebApplication app)
     {
         // ثبت مسیر POST برای ایجاد ژانر جدید
-        app.MapPost("/createGenre", async (Genre genre, IGenreService service) =>
+        app.MapPost("/createGenre", async (Genre genre, IGenreService service, IOutputCacheStore outputCacheStore) =>
         {
             try
             {
                 var id = await service.CreateAsync(genre);
+                await outputCacheStore.EvictByTagAsync("genres-get", default);
                 return Results.Created($"/createGenre/{id}", genre);
             }
             catch (ArgumentException ex)
@@ -27,7 +29,7 @@ public class GenreEndpoints
             {
                 return Results.Problem($"An unexpected error occurred: {ex.Message}");
             }
-        });
+        }).WithTags("Return Create Genre"); ;
 
         // ثبت مسیر GET برای دریافت لیست ژانرها
         app.MapGet("/genres", async (IGenreService service) =>
@@ -49,6 +51,29 @@ public class GenreEndpoints
             {
                 return Results.Problem($"An unexpected error occurred: {ex.Message}");
             }
-        });
+        }).WithTags("Return Get All Genre");
+
+        // ثبت مسیر GET برای دریافت یک ژانر
+        app.MapGet("/genre/{id:guid}", async (IGenreService service, Guid id) =>
+        {
+
+            try
+            {
+                var genre = await service.GetIdAsync(id);
+                return Results.Ok(genre);
+            }
+            catch (ArgumentException ex)
+            {
+                return Results.BadRequest(new { Message = ex.Message });
+            }
+            catch (InvalidOperationException ex)
+            {
+                return Results.Conflict(new { Message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem($"An unexpected error occurred: {ex.Message}");
+            }
+        }).WithTags("Return Get By Guid Genre");
     }
 }
--- EndPoints/EndPoint.Minimal.Api/Program.cs
+++ EndPoints/EndPoint.Minimal.Api/Program.cs
@@ -2,10 +2,15 @@ using EndPoint.Minimal.Api.Data;
 using EndPoint.Minimal.Api.EndPoints;
 using EndPoint.Minimal.Api.Services.GenreServices;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
+builder.Services.AddOutputCache(options =>
+{
+    options.AddBasePolicy(policy => policy.Expire(TimeSpan.FromMinutes(10)));
+});
 
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<ApplicationContext>(options =>
@@ -17,11 +22,12 @@ app.MapDefaultEndpoints();
 
 if (app.Environment.IsDevelopment())
 {
+    app.MapScalarApiReference();
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
+app.UseOutputCache();
 GenreEndpoints.MapEndpoints(app);
 
 
