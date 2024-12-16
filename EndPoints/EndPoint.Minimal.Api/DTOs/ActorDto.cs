31df30229e56c959ca1dfea828bb22ab3af974f2
discoverbasecode <discoverbasecode@gmail.com>
2024-12-16 21:43:56 +0330

    add dtos Actor and IActorServices

---
+++ EndPoints/EndPoint.Minimal.Api/DTOs/ActorDto.cs
@@ -0,0 +1,16 @@
+﻿namespace EndPoint.Minimal.Api.DTOs;
+
+public class CreateActorDto
+{    
+    public string FullName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Picture { get; set; }
+}
+
+public class UpdateActorDto : CreateActorDto
+{
+    public Guid Id { get; set; }
+}
+
+public class GetAllActorDto : UpdateActorDto;
+public class GetByIdActorDto : GetAllActorDto;
