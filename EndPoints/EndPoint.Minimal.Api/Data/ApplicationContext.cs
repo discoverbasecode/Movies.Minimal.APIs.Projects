49c650f592641e1f278eb59b9318238fa1c9c7e2
discoverbasecode <discoverbasecode@gmail.com>
2024-12-16 21:43:14 +0330

    add Model Actor and to Context

--- EndPoints/EndPoint.Minimal.Api/Data/ApplicationContext.cs
+++ EndPoints/EndPoint.Minimal.Api/Data/ApplicationContext.cs
@@ -6,6 +6,7 @@ namespace EndPoint.Minimal.Api.Data;
 public class ApplicationContext(DbContextOptions<ApplicationContext> options) : DbContext(options)
 {
     public DbSet<Genre> Genres { get; set; }
+    public DbSet<Actor> Actors { get; set; }
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -13,6 +14,7 @@ public class ApplicationContext(DbContextOptions<ApplicationContext> options) : 
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.Entity<Genre>().Property(c => c.Name).HasMaxLength(200).IsRequired();
+        modelBuilder.Entity<Actor>().Property(c => c.Picture).IsUnicode();
 
     }
 }
---
+++ EndPoints/EndPoint.Minimal.Api/Model/Actor.cs
@@ -0,0 +1,9 @@
+﻿namespace EndPoint.Minimal.Api.Model;
+
+public class Actor(string fullName, DateTime dateOfBirth, string picture)
+{
+    public Guid Id { get; set; } = new();
+    public string FullName { get; set; } = fullName;
+    public DateTime DateOfBirth { get; set; } = dateOfBirth;
+    public string Picture { get; set; } = picture;
+}
